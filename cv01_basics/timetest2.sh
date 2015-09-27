6b1cde4bc9098f381cbd44ef8cf4a3441b9cd13a
Vítězslav Grygar <lightfaith@users.noreply.github.com>
2015-09-27 17:25:12 +0200

    Final timing tests and results

--- cv01_basics/timetest2.sh
+++ cv01_basics/timetest2.sh
@@ -4,8 +4,8 @@
 size=32768 # number of ints to sort
 echo "Running ./sort for $size random numbers";
 
-for (( i=64; $i<=256; i=i+1 )); do
-	# run ./sort for SIZE numbers and 64-256 processes, measure real time
+for (( i=32; $i<=128; i=i+1 )); do
+	# run ./sort for SIZE numbers and 32-128 processes, measure real time
 	echo -n "$i processes: ";
 	t=`(time ./sort $size $i > /dev/null) 2>&1 | grep real | cut -d'	' -f2`;
 	sleep 0.05
