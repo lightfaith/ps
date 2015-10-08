516cb2d16755c7aacb24082f4c39c11baa5fc4e7
Vítězslav Grygar <lightfaith@users.noreply.github.com>
2015-10-08 11:27:54 +0200

    Task03 - fork() & MOSIX comparation

---
+++ cv03_cluster/bench.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+processes=32;
+size=1000;
+maxsize=10000000;
+
+echo "Running sort in $processes processes."
+for (( i=$size; $i<=$maxsize; i*=2 )); do
+	echo -n "$i elements: ";
+	t=`(time (mosrun ./sort $i $processes > /dev/null)) 2>&1`;
+	echo `echo "$t" | grep "real" | cut -d '	' -f2`;
+done
